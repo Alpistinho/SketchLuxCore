bf76aa97ff9cc863cfc6d30657b1443105db9974
Daniel Moreira Guimarães <danielmguimaraes@poli.ufrj.br>
2018-07-08 22:37:36 -0300

    Toying with Sketchup API

--- SketchLux.rb
+++ SketchLux.rb
@@ -9,55 +9,26 @@
 #               	Mimmo Briganti (aka mimhotep)
 
 require 'sketchup.rb'
-#TODO implement fallback for Ruby 1.8 (used by Sketchup 8)
-require_relative 'storage/materialstorage.rb'
 
-require_relative 'sketchupInterface/sketchLuxToolbar.rb'
-require_relative 'sketchupInterface/sketchLuxMainMenu.rb'
-require_relative 'sketchupInterface/materialeditor.rb'
-require_relative 'sketchupInterface/enginesettingseditor.rb'
-
-
-module SketchLux
-
-	#These hashes should be used to store the Storage's objects that hold the Luxrender scene objects.
-	#Each Sketchup active_model should have one storage of each type linked to it
-	#The key should be the active_model names
-	materialStorages = Hash.new
-	textureStorages = Hash.new
-	shapeStorages = Hash.new
-	volumeStorages = Hash.new
-	lightStorages = Hash.new
-
-	#Hash holding all Storages of each type
-	storages = {"material" => materialStorages, "texture" => textureStorages, "shape" => shapeStorages, "volume" => volumeStorages, "light" => volumeStorages}
-
-	#Used to store the observers
-	#keys should be the active_model names
-	observers = Hash.new
-
-
-	#These hashes should be used to store the settings.
-	#Each Sketchup active_model should have one setting of each type linked to it
-	#The key should be the active_model names
-	samplerSettings = Hash.new
-	engineSettings = Hash.new
-	cameraSettings = Hash.new
-	filterSettings = Hash.new
-	filmSettings = Hash.new
-
-	#Hash holding all settings
-	settings = {"engine" => engineSettings, "sampler" => samplerSettings, "camera" => cameraSettings, "filter" => filmSettings, "film" => filmSettings}
+module SketchLuxCore
 
 	#get the model currently focused by the user
 	activeModel = Sketchup.active_model
-
-	#creates the editors that control the user interface
-	materialEditor = MaterialEditor.new(activeModel, materialStorages)
-	engineSettingsEditor = EngineSettingsEditor.new(activeModel, engineSettings)
-
+    
 	#creates the SketchLux toolbar using the editor sent in the parameters
-	toolbar = SketchLux::createToolbar(materialEditor, engineSettingsEditor)
-	mainMenu = SketchLux::mainMenu()
+	main_menu = UI.menu("Plugins").add_submenu("LuxRender")
+    
+    modelEntities = activeModel.entities
+    face_count = 0
+    modelEntities.each { |entity|
+      if entity.is_a? Sketchup::Face
+        faceMesh = entity.mesh
+        facePoints = faceMesh.points
+        puts facePoints
+        face_count = face_count + 1
+      end
+    }
 
+    UI.messagebox("There are " + face_count.to_s + " faces selected.")
+    
 end
